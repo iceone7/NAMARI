6248860d5f164a2c8325b28be46503a03b6029c8
pavlik <107913419+iceone7@users.noreply.github.com>
2024-07-21 13:03:38 +0400

    NAMARI

---
+++ js/hamburger-menu.js
@@ -0,0 +1,8 @@
+function myFunction() {
+    var x = document.getElementById("center-header");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
---
+++ scroll-animation.js
@@ -0,0 +1,9 @@
+ScrollReveal({ distance: '10px' });
+ScrollReveal().reveal(".catalog", { interval: 200 });
+ScrollReveal().reveal(".user", { interval: 200 });
+ScrollReveal().reveal(".left-header", { origin: 'left' });
+ScrollReveal().reveal(".right-header", { origin: 'right' });
+ScrollReveal().reveal(".images-1-3", { interval: 100 });
+ScrollReveal().reveal(".images-3-6", { interval: 100 });
+
+
